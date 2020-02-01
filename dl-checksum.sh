0e098e1770bbe2f8871353063aacbeba3e9cba5f
Andrew Rothstein <andrew.rothstein@gmail.com>
2020-02-01 16:50:42 -0500

    up to v1.2.0

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -1,28 +1,37 @@
 #!/usr/bin/env sh
-VER=v1.1.14
-BUILDHASH=874754de
+set -e
 DIR=~/Downloads
 MIRROR=https://github.com/openshift/source-to-image/releases/download
 
 dl()
 {
-    OS=$1
-    PLATFORM=$2
-    SUFFIX=$3
-    FILE=source-to-image-$VER-$BUILDHASH-$OS-$PLATFORM.$SUFFIX
-    DLFILE=$DIR/$FILE
-    URL=$MIRROR/$VER/$FILE
-    if [ ! -e $DLFILE ]
+    local ver=$1
+    local build_hash=$2
+    local os=$3
+    local arch=$4
+    local suffix=${5:-tar.gz}
+    local platform="${os}-${arch}"
+    local file=source-to-image-$ver-$build_hash-$platform.$suffix
+    local lfile=$DIR/$file
+    local url=$MIRROR/$ver/$file
+    if [ ! -e $lfile ]
     then
-        wget -q -O $DLFILE $URL
+        wget -q -O $lfile $url
     fi
-    printf "  # %s\n  %s-%s: sha256:%s\n" $URL $OS $PLATFORM `sha256sum $DLFILE | awk '{print $1}'`
+    printf "    # %s\n" $url
+    printf "    %s: sha256:%s\n" $platform $(sha256sum $lfile | awk '{print $1}')
 }
 
-printf "%s-%s:\n" $VER $BUILDHASH
-dl darwin amd64 tar.gz
-dl linux 386 tar.gz
-dl linux amd64 tar.gz
-dl windows amd64 zip
+dl_ver() {
+    local ver=$1
+    local build_hash=$2
 
+    printf "  %s-%s:\n" $ver $build_hash
+    dl $ver $build_hash darwin amd64
+    dl $ver $build_hash linux 386
+    dl $ver $build_hash linux amd64
+    dl $ver $build_hash windows amd64 zip
+}
+
+dl_ver v1.2.0 2a579ecd
 
